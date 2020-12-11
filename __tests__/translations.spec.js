591e670b13ce2b95c9dca8d8eb28687d90ce705f
Rhian Castro <rhi.castro@gmail.com>
2020-12-11 01:22:48 -0300

    cleaned up some code

--- __tests__/translations.spec.js
+++ __tests__/translations.spec.js
@@ -4,8 +4,7 @@ const { HTTP_PORT } = process.env;
 const port = HTTP_PORT || 8080;
 const baseURL = `http://localhost:${port}/movies/translations`;
 const mongoConnection = require('mongoose');
-const db_url = `mongodb://localhost:27017/ilia`
-
+const db_url = `mongodb://localhost:27017/ilia`;
 
 const options = {
   useNewUrlParser: true,
@@ -17,36 +16,37 @@ beforeAll(async () => {
   await mongoConnection.connect(db_url, options);
   await mongoConnection.connection.dropDatabase();
   await mongoConnection.connection.close();
-})
-
+});
 
 it('all other endpoits must be 404:', () => {
-  return frisby.get(baseURL)
-    .expect('status', 404);
+  return frisby.get(baseURL).expect('status', 404);
 });
 
 it('should properly handle unknown resources from TMDB and return an error message.', () => {
-  return frisby.get(`${baseURL}/23`)
+  return frisby
+    .get(`${baseURL}/23`)
     .expect('status', 404)
-    .expect('json', {error: "Resource not found"});
+    .expect('json', { error: 'Resource not found' });
 });
 
 it('when searching for a resource for the first time, it must be saved to DB and then, returned.', async () => {
-  await frisby.get(`${baseURL}/414`)
+  await frisby
+    .get(`${baseURL}/414`)
     .expect('status', 201)
     .then((response) => {
       const { json } = response;
-      expect(json.id).toBe(414)
+      expect(json.id).toBe(414);
       expect(json.translations[0].data.title).toBe('Батман завинаги');
     });
 });
 
 it('when searching for a resource from the second time, it must be returned directly from the db.', async () => {
-  await frisby.get(`${baseURL}/414`)
+  await frisby
+    .get(`${baseURL}/414`)
     .expect('status', 200)
     .then((response) => {
       const { json } = response;
-      expect(json.id).toBe(414)
+      expect(json.id).toBe(414);
       expect(json.translations[0].data.title).toBe('Батман завинаги');
-    })
-  });
+    });
+});
